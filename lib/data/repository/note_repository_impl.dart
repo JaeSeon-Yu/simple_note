6995dfc4c5a9c7dc38fad2048dd7b9ab7cd37a09
JaeSeon-Yu <stacey5165@naver.com>
2022-10-26 16:05:30 +0900

    db 추가

---
+++ lib/data/repository/note_repository_impl.dart
@@ -0,0 +1,36 @@
+import 'package:simple_note/data/data_source/note_db.dart';
+import 'package:simple_note/domain/model/note.dart';
+import 'package:simple_note/domain/repository/note_repository.dart';
+
+class NoteRepositoryImpl extends NoteRepository{
+  final NoteDB db;
+
+  NoteRepositoryImpl(this.db);
+
+  @override
+  Future<void> deleteNote(Note note) async {
+    await db.deleteNote(note);
+  }
+
+  @override
+  Future<Note?> getNoteById(int id) async {
+    return await db.getNoteById(id);
+  }
+
+  @override
+  Future<List<Note>> getNotes() async {
+    return await db.getNotes();
+  }
+
+  @override
+  Future<void> insertNote(Note note) async {
+    await db.insertNote(note);
+  }
+
+  @override
+  Future<void> updateNote(Note note) async{
+    await db.updateNote(note);
+  }
+
+
+}
